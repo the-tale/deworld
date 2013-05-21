b712ccdb79b6f719934fefc8fff091bee4a7e1cc
Tiendil <a.eletsky@gmail.com>
2013-05-21 22:31:37 +0300

    code for collecting power's delta on cell with specified coordinates

--- deworld/power_points/base_point.py
+++ deworld/power_points/base_point.py
@@ -2,6 +2,21 @@
 
 from deworld.layers import LAYER_TYPE
 
+from deworld.layers import BaseLayer
+
+class LogLayer(BaseLayer):
+
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+        self.value = None
+
+    def add_power(self, x ,y, power):
+        if x == self.x and y == self.y:
+            self.value = power
+
+
 class BasePoint(object):
 
     def __init__(self, layer_type, name, x, y, power, default_power=0.0):
@@ -24,6 +39,11 @@ class BasePoint(object):
             self.update_powers(world.layer_vegetation, world)
         elif self.layer_type == LAYER_TYPE.SOIL:
             self.update_powers(world.layer_soil, world)
+
+    def log_powers_for(self, world, x, y):
+        logger = LogLayer(world, x, y)
+        self.update_powers(logger, world)
+        return logger.value
 
     def update_powers(self, layer, world):
         pass
